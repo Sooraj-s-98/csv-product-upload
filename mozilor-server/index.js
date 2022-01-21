2b938ff12048bc92e5a5bbb005786d3f2aa97007
Sooraj S <52352285+Sooraj-s-98@users.noreply.github.com>
2022-01-21 15:44:53 +0530

    upload pending

--- mozilor-server/index.js
+++ mozilor-server/index.js
@@ -7,25 +7,32 @@ const errorHandler = require('_middleware/error-handler');
 const authorize = require('_middleware/authorize');
 
 global.__basedir = __dirname + "/..";
+
   app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:3000");
-    res.header("Access-Control-Allow-Credentials", true);
-   // res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE,OPTIONS");
-    res.header(
-        "Access-Control-Allow-Headers",
-        "Origin, X-Requested-With, Content-Type, Accept"
-    );
+  //   res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  //   res.header("Access-Control-Allow-Credentials", true);
+  //   res.header("Access-Control-Allow-Methods", "GET, POST, PUT ,DELETE,OPTIONS");
+  //   res.header(
+  //       "Access-Control-Allow-Headers",
+  //       "Origin, X-Requested-With, Content-Type, Accept"
+  //   );
+  //   if (req.method === "OPTIONS") {
+  //     return res.status(200).end();
+  // }
     next();
 });
-// app.use(cors())
+var corsOptions = {
+  credentials: true,
+  origin: "http://localhost:3000",
+  optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+}
+app.use(cors(corsOptions))
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-
-
 // api routes
 app.use('/users', require('./users/users.controller'));
-app.use('/product',authorize(),  require('./product/product.routes') );
+app.use('/product', cors(corsOptions),authorize(),  require('./product/product.routes'));
 
 // global error handler
 app.use(errorHandler);
